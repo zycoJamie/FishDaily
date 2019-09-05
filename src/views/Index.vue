d7c247f2d8b5635e567a54a7dbb0e531e994e718
zi <zycojamie@163.com>
2019-09-05 17:14:44 +0800

    使用路过图床，测试图床

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,6 +1,6 @@
 <template>
-    <div>
-        777
+    <div class="content" :style="backgroundObj">
+        
     </div>
 </template>
 
@@ -10,7 +10,23 @@ export default {
     name:"Index",
     data(){
         return {
-
+            background:{
+                height:'100%',
+                width:'100%',
+                backgroundImage:'',
+                backgroundRepeat:'no-repeat',
+                backgroundAttachment:'fixed',
+                backgroundPosition:'center'
+            },
+            bgList:[
+                'https://s2.ax1x.com/2019/09/05/nnkmq0.jpg'
+            ]
+        }
+    },
+    computed:{
+        backgroundObj(){
+            this.background['backgroundImage']=`url(${this.bgList[0]})`
+            return this.background
         }
     },
     mounted(){
@@ -20,5 +36,5 @@ export default {
 </script>
 
 <style lang='scss'>
-
+    
 </style>
