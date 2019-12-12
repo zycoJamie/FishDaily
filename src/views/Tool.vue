b1b798ed2605dd4be28b9d404e4b189cbbf6bc66
zycoJamie <410752387@qq.com>
2019-12-12 22:41:02 +0800

    coding 百宝袋模块

--- src/views/Tool.vue
+++ src/views/Tool.vue
@@ -1,6 +1,50 @@
 <template>
-    <div>
-        Tool
+    <div class="center">
+        <div class="col-4">
+            <a-row :gutter="{ xs: 8, sm: 16, md: 24, lg: 32 }">
+                <a-col :span="6">
+                    <a-card :bordered="true" hoverable class="card">
+                        <div :style="{textAlign:'center'}"><i class="iconfont">&#xe859;</i></div>
+                        <p :style="{textAlign:'center'}">飞机大战</p>
+                    </a-card>
+                </a-col>
+                <a-col :span="6">
+                    <a-card :bordered="true" hoverable class="card">
+                        <div :style="{textAlign:'center'}"><i class="iconfont">&#xe859;</i></div>
+                        <p :style="{textAlign:'center'}">飞机大战</p>
+                    </a-card>
+                </a-col>
+                <a-col :span="6">
+                    <a-card :bordered="true" hoverable class="card">
+                        <div :style="{textAlign:'center'}"><i class="iconfont">&#xe859;</i></div>
+                        <p :style="{textAlign:'center'}">飞机大战</p>
+                    </a-card>
+                </a-col>
+                <a-col :span="6">
+                    <a-card :bordered="true" hoverable class="card">
+                        <div :style="{textAlign:'center'}"><i class="iconfont">&#xe859;</i></div>
+                        <p :style="{textAlign:'center'}">飞机大战</p>
+                    </a-card>
+                </a-col>
+                
+            </a-row>
+        </div>
+        <div class="col-2">
+            <a-row :gutter="{ xs: 8, sm: 16, md: 24, lg: 32 }">
+                <a-col :span="12">
+                    <a-card :bordered="true" hoverable class="card">
+                        <div :style="{textAlign:'center'}"><i class="iconfont">&#xe859;</i></div>
+                        <p :style="{textAlign:'center'}">飞机大战</p>
+                    </a-card>
+                </a-col>
+                <a-col :span="12">
+                    <a-card :bordered="true" hoverable class="card">
+                        <div :style="{textAlign:'center'}"><i class="iconfont">&#xe859;</i></div>
+                        <p :style="{textAlign:'center'}">飞机大战</p>
+                    </a-card>
+                </a-col>
+            </a-row>
+        </div>
     </div>
 </template>
 
@@ -10,6 +54,35 @@ export default{
 }
 </script>
 
-<style lang="scss">
-    
+<style lang="scss" scoped>
+@import "~style/global.scss";
+$f-size:.4rem;
+    .center{
+        width: 80vw;
+        margin: .5rem auto;
+        .card{
+            font-size: $f-size;
+            border-radius: 10px;
+            /deep/ .ant-card-body{
+                padding: 15px;
+            }
+            .iconfont{
+                font-size: $f-size+.4rem;
+                color: $theme-color;
+            }
+        }
+        .col-4{
+            @media screen and (max-width:620px){
+                display: none;
+            }
+            @media screen and (width:620px){
+                display: block;
+            }
+        }
+        .col-2{
+            @media screen and (min-width:620px) {
+                display: none;
+            }
+        }
+    }
 </style>
